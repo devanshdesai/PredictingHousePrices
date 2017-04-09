5f6d1ed68de4d64e9c30dede27d3a32474e96f56
westenweiss <westenweiss@gmail.com>
2017-04-09 12:29:16 -0400

    KNN Plot w/ Kaggle Measure of fit

--- Code Snippets/KNN Code.R
+++ Code Snippets/KNN Code.R
@@ -9,3 +9,9 @@ for (i in 1:10) {
   filename = paste("KNN Predictions", toString(i*2), sep = "")
   write.csv(knn.pred, file = paste(filename, ".csv", sep = ""))
 }
+
+plot(knn.pred, type = "o", col="lightblue")
+
+k_values=seq(2, 20, 2)
+kaggle_scores=c(0.35927, 0.36552, 0.34928, 0.34711, 0.34132, 0.33861, 0.33943, 0.33810, 18, 0.33765)
+qplot(k_values, kaggle_scores, main = "KNN Predictions - Kaggle Scores vs K Values", ylab = "Kaggle Score (RMSE)", xlab = "K Values")
