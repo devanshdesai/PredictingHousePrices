0d21284ec76a2fbeea31164e75183f0ab42aa8cb
johnpalmieri <johnpalmieri@ty-wireless-pittnet-150-212-32-113.wireless.pitt.edu>
2017-04-10 18:51:46 -0400

    Fixed one error, ran into another

---
+++ Data/Ridge.R
@@ -0,0 +1,13 @@
+library(glmnet)
+data <- read.csv("trainy_processed.csv")
+test <- read.csv('test_processed.csv')
+x <- data[,c(1:213)]
+y <- data[,214]
+x[] <- lapply(x, function(x) as.numeric(as.character(x)))
+grid=10^seq(10,-2,length=100)
+ridge.mod=glmnet(x,y,alpha=0,lambda=grid)
+dim(coef(ridge.mod))
+ridge.mod$lambda [50]
+coef(ridge.mod)[,50]
+ridge.mod=glmnet(x[housedata,],y[housedata],alpha=0,lambda=grid, thresh =1e -12)
+ridge.mod=glmnet(x[housedata,],y[housedata],alpha=0, lambda=grid,thresh=1e-12)
