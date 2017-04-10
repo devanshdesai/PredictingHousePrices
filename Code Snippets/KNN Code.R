92b78a7f401c1559f7d3cbdf8df3e93009ed87da
westenweiss <westenweiss@gmail.com>
2017-04-09 20:22:53 -0400

    Updated Kaggle Score point

--- Code Snippets/KNN Code.R
+++ Code Snippets/KNN Code.R
@@ -13,5 +13,5 @@ for (i in 1:10) {
 plot(knn.pred, type = "o", col="lightblue")
 
 k_values=seq(2, 20, 2)
-kaggle_scores=c(0.35927, 0.36552, 0.34928, 0.34711, 0.34132, 0.33861, 0.33943, 0.33810, 18, 0.33765)
+kaggle_scores=c(0.35927, 0.36552, 0.34928, 0.34711, 0.34132, 0.33861, 0.33943, 0.33810, 0.33806, 0.33765)
 qplot(k_values, kaggle_scores, main = "KNN Predictions - Kaggle Scores vs K Values", ylab = "Kaggle Score (RMSE)", xlab = "K Values")
