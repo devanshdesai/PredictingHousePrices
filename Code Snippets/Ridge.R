a516ffbe37a9ae0e4e81a3477e767d5c0924087e
johnpalmieri <johnpalmieri@ty-wireless-pittnet-150-212-32-113.wireless.pitt.edu>
2017-04-10 22:12:43 -0400

    Ridge and Lasso Code

--- Code Snippets/Ridge.R
+++ Code Snippets/Ridge.R
@@ -3,22 +3,17 @@ data <- read.csv("../Data/trainy_processed.csv")
 test <- read.csv('../Data/test_processed.csv')
 x <- data[,1:213]
 y <- data[,214]
-fc <- sapply(names(x),function(x){class(x)})
-fc
-x <- lapply(x,function(x) as.numeric(as.character(x)))
-fc <- sapply(names(x),function(x){class(x)})
-fc
-grid=10^seq(10,-2,length=100)
-ridge.mod=glmnet(x,y,alpha=0,lambda=grid)
-dim(coef(ridge.mod))
-ridge.mod$lambda [50]
-coef(ridge.mod)[,50]
-ridge.mod=glmnet(x[housedata,],y[housedata],alpha=0,lambda=grid, thresh =1e -12)
-ridge.mod=glmnet(x[housedata,],y[housedata],alpha=0, lambda=grid,thresh=1e-12)
-
 
 library(glmnet)
 train = read.csv("../Data/trainy_processed.csv")
 train = train[,-1]
+test = test[, -1]
 grid = 10^seq(10,-2,length=100)
-model = cv.glmnet(as.matrix(train[, 1:212]), as.double(train[,213]), alpha = 0, lambda = grid)
+cvmodel = cv.glmnet(as.matrix(train[, 1:212]), as.double(train[,213]), alpha = 0, lambda = grid)
+model = glmnet(as.matrix(train[, 1:212]), as.double(train[,213]), alpha = 0, lambda = cvmodel$lambda.min)
+ridge.pred=predict(model,as.matrix(test))
+SalePrice=exp(ridge.pred)
+ID=1461:2919
+c=cbind(ID,SalePrice)
+write.csv(c,file = "ridgepred4.csv")
+
--- Data/Lasso.R
+++ Data/Lasso.R
@@ -1,6 +1,19 @@
 library(glmnet)
-data <- read.csv("trainy_processed.csv")
-test <- read.csv('test_processed.csv')
-x <- data[,c(1:213)]
+data <- read.csv("../Data/trainy_processed.csv")
+test <- read.csv('../Data/test_processed.csv')
+x <- data[,1:213]
 y <- data[,214]
-lasso.mod=glmnet(x,y,alpha=1,lambda=grid)
+
+library(glmnet)
+train = read.csv("../Data/trainy_processed.csv")
+train = train[,-1]
+test = test[, -1]
+grid = 10^seq(10,-2,length=100)
+cvmodel = cv.glmnet(as.matrix(train[, 1:212]), as.double(train[,213]), alpha = 1, lambda = grid)
+model = glmnet(as.matrix(train[, 1:212]), as.double(train[,213]), alpha = 1, lambda = cvmodel$lambda.min)
+lasso.pred=predict(model,as.matrix(test))
+SalePrice=exp(lasso.pred)
+ID=1461:2919
+c=cbind(ID,SalePrice)
+write.csv(c,file = "lassopred1.csv")
+
