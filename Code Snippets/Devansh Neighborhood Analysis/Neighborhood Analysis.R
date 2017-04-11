b140157517416b1e1206e2ace67e87d849dabb29
Devansh Desai <devanshdesai@hotmail.com>
2017-04-11 01:25:10 -0400

    Updated plots for neighborhood analysis

--- Code Snippets/Devansh Neighborhood Analysis/Neighborhood Analysis.R
+++ Code Snippets/Devansh Neighborhood Analysis/Neighborhood Analysis.R
@@ -13,28 +13,28 @@ df = na.omit(df[order(df$SalePrice),])
 df$Neighborhood = as.vector(df$Neighborhood)
 df$Neighborhood = factor(df$Neighborhood, df$Neighborhood)
 
-qplot(df$Neighborhood, df$SalePrice, ylab = "Average Sale Price", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Sale Price vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$SalePrice)) + labs(colour = "Sale Price")
+qplot(df$Neighborhood, df$SalePrice, ylab = "Average Sale Price", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Sale Price vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$SalePrice)) + labs(colour = "Sale Price")
 
-qplot(df$Neighborhood, df$TotalSF, ylab = "Average Square Footage", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Square Footage vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$TotalSF)) + labs(colour = "Square Footage")
+qplot(df$Neighborhood, df$TotalSF, ylab = "Average Square Footage", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Square Footage vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$TotalSF)) + labs(colour = "Square Footage")
 cor(df$SalePrice, df$TotalSF)
 
-qplot(df$Neighborhood, df$LotArea, ylab = "Average Lot Area", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Lot Area vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$LotArea)) + labs(colour = "Lot Area")
+qplot(df$Neighborhood, df$LotArea, ylab = "Average Lot Area", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Lot Area vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$LotArea)) + labs(colour = "Lot Area")
 cor(df$SalePrice, df$LotArea)
 
-qplot(df$Neighborhood, df$OverallQual, ylab = "Average Overall Quality", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Overall Quality vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$OverallQual)) + labs(colour = "Overall Quality")
+qplot(df$Neighborhood, df$OverallQual, ylab = "Average Overall Quality", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Overall Quality vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$OverallQual)) + labs(colour = "Overall Quality")
 cor(df$SalePrice, df$OverallQual)
 
-qplot(df$Neighborhood, df$Fireplaces, ylab = "Average Fireplaces", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Fireplaces vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$Fireplaces)) + labs(colour = "Fireplaces")
+qplot(df$Neighborhood, df$Fireplaces, ylab = "Average Fireplaces", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Fireplaces vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$Fireplaces)) + labs(colour = "Fireplaces")
 cor(df$SalePrice, df$Fireplaces)
 
-qplot(df$Neighborhood, df$BedroomAbvGr, ylab = "Average Bedrooms", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Bedrooms vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$BedroomAbvGr)) + labs(colour = "Bedrooms")
+qplot(df$Neighborhood, df$BedroomAbvGr, ylab = "Average Bedrooms", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Bedrooms vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$BedroomAbvGr)) + labs(colour = "Bedrooms")
 cor(df$SalePrice, df$BedroomAbvGr)
 
-qplot(df$Neighborhood, df$GarageCars, ylab = "Average Garage Size", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Garage Size vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$GarageCars)) + labs(colour = "Garage Size")
+qplot(df$Neighborhood, df$GarageCars, ylab = "Average Garage Size", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Garage Size vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$GarageCars)) + labs(colour = "Garage Size")
 cor(df$SalePrice, df$GarageCars)
 
-qplot(df$Neighborhood, df$TotalBath, ylab = "Average Bathrooms", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Bathrooms vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$TotalBath)) + labs(colour = "Bathrooms")
+qplot(df$Neighborhood, df$TotalBath, ylab = "Average Bathrooms", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Bathrooms vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$TotalBath)) + labs(colour = "Bathrooms")
 cor(df$SalePrice, df$TotalBath)
 
-qplot(df$Neighborhood, df$YearBuilt, ylab = "Average Year Built", xlab = "Neighborhood (Desc by Average Sale Price)", main = "Average Year Built vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$YearBuilt)) + labs(colour = "Year Built")
+qplot(df$Neighborhood, df$YearBuilt, ylab = "Average Year Built", xlab = "Neighborhood (Asc by Average Sale Price)", main = "Average Year Built vs Neighborhood") +  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + geom_point(aes(colour = df$YearBuilt)) + labs(colour = "Year Built")
 cor(df$SalePrice, df$YearBuilt)
