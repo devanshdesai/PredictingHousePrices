4df229822ed0325f4843a15b47b20f1c5c5697b3
Devansh Desai <devanshdesai@hotmail.com>
2017-04-10 20:37:58 -0400

    Worked on ridge regression for John

--- Code Snippets/Ridge.R
+++ Code Snippets/Ridge.R
@@ -1,7 +1,7 @@
 library(glmnet)
-data <- read.csv("trainy_processed.csv")
-test <- read.csv('test_processed.csv')
-x <- data[,c(1:213)]
+data <- read.csv("../Data/trainy_processed.csv")
+test <- read.csv('../Data/test_processed.csv')
+x <- data[,1:213]
 y <- data[,214]
 fc <- sapply(names(x),function(x){class(x)})
 fc
@@ -15,3 +15,10 @@ ridge.mod$lambda [50]
 coef(ridge.mod)[,50]
 ridge.mod=glmnet(x[housedata,],y[housedata],alpha=0,lambda=grid, thresh =1e -12)
 ridge.mod=glmnet(x[housedata,],y[housedata],alpha=0, lambda=grid,thresh=1e-12)
+
+
+library(glmnet)
+train = read.csv("../Data/trainy_processed.csv")
+train = train[,-1]
+grid = 10^seq(10,-2,length=100)
+model = cv.glmnet(as.matrix(train[, 1:212]), as.double(train[,213]), alpha = 0, lambda = grid)
